7cd81b7964e51407519dbe008f127d86e396b709
CeliaGarcia <celia.garcia@ikasle.egibide.org>
2022-01-28 10:59:41 +0100

    Trabajo hecho en clase

--- TEMA7/EntornoGraficoEjercicio2/src/MODELO/Cursos.java
+++ TEMA7/EntornoGraficoEjercicio2/src/MODELO/Cursos.java
@@ -1,4 +1,43 @@
 package MODELO;
 
+import java.util.ArrayList;
+
 public class Cursos {
+    String codigoCurso;
+    ArrayList<Alumnos> listaAlumnos;
+
+    public Cursos(String codigoCurso, ArrayList<Alumnos> listaAlumnos) {
+        this.codigoCurso = codigoCurso;
+        this.listaAlumnos = listaAlumnos;
+        listaAlumnos= new ArrayList<>();
+    }
+
+    public Cursos(String codigoCurso) {
+        this.codigoCurso = codigoCurso;
+    }
+
+    public String getCodigoCurso() {
+        return codigoCurso;
+    }
+
+    public void setCodigoCurso(String codigoCurso) {
+        this.codigoCurso = codigoCurso;
+    }
+
+    public ArrayList<Alumnos> getListaAlumnos() {
+        return listaAlumnos;
+    }
+
+    public void setListaAlumnos(ArrayList<Alumnos> listaAlumnos) {
+        this.listaAlumnos = listaAlumnos;
+    }
+    //añadir borrar alumno
+
+    @Override
+    public String toString() {
+        return "Cursos{" +
+                "codigoCurso='" + codigoCurso + '\'' +
+                ", listaAlumnos=" + listaAlumnos +
+                '}';
+    }
 }
--- TEMA7/EntornoGraficoEjercicio2/src/VISTA/V1.java
+++ TEMA7/EntornoGraficoEjercicio2/src/VISTA/V1.java
@@ -1,7 +1,82 @@
 package VISTA;
 
+import com.company.Main;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
 
 public class V1 {
-    private JPanel jpTitulo;
+
+    private JPanel jpPrincipal;
+    private JLabel lTitulo;
+    private JLabel lApellido;
+    private JTextField tapellido;
+    private JLabel lNombre;
+    private JTextField tDnia;
+    private JButton bAceptar;
+    private JButton bSalir;
+    private JLabel lCurso;
+    private JTextField tCurso;
+    private JLabel lDni;
+    private JTextField tDni;
+
+    private boolean elAlumnoExiste;
+
+    public JPanel getJpPrincipal() {
+        return jpPrincipal;
+    }
+
+
+
+    public V1() {
+        //Lo primero que pone es el dni. Cuando se quita del DNI¿que hace?-> Valida DNI
+        tDni.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusLost(FocusEvent e) {
+                super.focusLost(e);
+                validarDni();// En esta funcion compruebo valido el formato y busco si el alumno existe.
+                //Si el alumno no existe:
+
+                //Si el alumno existe:
+                if(elAlumnoExiste){
+                    //Si exite paso a validar el resto de datos
+                    validarNombre();
+                    validarApellido();
+                }
+
+            }
+        });
+
+    }
+
+    public void main(String[] args) {
+        JFrame frame = new JFrame("V1");
+        frame.setContentPane(new V1().jpPrincipal);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public void validarApellido(){}
+    public void validarDni() {
+        try {
+            if (tDnia.getText().isEmpty()) {
+                throw new Exception();
+            } else {
+                elAlumnoExiste = Main.comprobarSiExisteAlumno(tDnia.getText());
+                if(elAlumnoExiste){
+
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e.getClass());
+        }
+    }
+    public void validarNombre(){
+
+    }
+
 }
