257d26427aa22a1ba7aadca047e773c081a23b4c
Celia Garcia <celia.garcia@ikasle.egibide.org>
2022-02-02 16:01:51 +0100

    EJERCICIO TERMINADO

--- TEMA7/ENTORNOGRAFICO_EJERCICIO3/src/VISTA/VentanaCompra.java
+++ TEMA7/ENTORNOGRAFICO_EJERCICIO3/src/VISTA/VentanaCompra.java
@@ -1,5 +1,6 @@
 package VISTA;
 
+import MODELO.Productos;
 import com.company.Main;
 
 import javax.swing.*;
@@ -7,6 +8,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
+
+import static java.lang.Float.valueOf;
 
 public class VentanaCompra {
     private JPanel panelPrincipal;
@@ -19,6 +22,7 @@ public class VentanaCompra {
     private JLabel lImporte;
     private JButton bAceptar;
     private JButton bCancelar;
+    static Productos prodcutoSeleccionado;
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("VentanaCompra");
@@ -37,6 +41,8 @@ public class VentanaCompra {
         bAceptar.addActionListener(new ActionListener() { /*Lo que hara cuando se de a aceptar. Validar importe y cantidad*/
             @Override
             public void actionPerformed(ActionEvent e) {
+                Main.actualizarStock (Integer.parseInt(tUnidades.getText()),Float.parseFloat (tImporte.getText()));
+                Main.mostrarStockActualizado(Integer.parseInt(tUnidades.getText()),Float.parseFloat(tImporte.getText()));
                 Main.mostrarVentanaOpciones();
             }
         });
@@ -46,18 +52,41 @@ public class VentanaCompra {
                 Main.mostrarVentanaOpciones();
             }
         });
-        tProducto.addFocusListener(new FocusAdapter() {/*Lo que hace cuando se pone el cursor en producto.  No se si
-        lo usaré*/
-            @Override
-            public void focusGained(FocusEvent e) {
-                super.focusGained(e);
-            }
-
-            @Override
-            public void focusLost(FocusEvent e) {/*Lo que hace si se quita el foco de producto. Lo usaré para que valide
+        tProducto.addFocusListener(new FocusAdapter() {/*Lo que hace si se quita el foco de producto. Lo usaré para que valide
             si el producto existe en el array LISTADEPRODUCTOS*/
+            @Override
+            public void focusLost(FocusEvent e) {
                 super.focusLost(e);
+                validarProducto();
+                Main.comprobarProducto(tProducto.getText());
             }
         });
+        tUnidades.addFocusListener(new FocusAdapter() {/*Cuando escriban las unidades que se van a comprar. Se suma al
+        stock de ese producto*/
+            @Override
+            public void focusLost(FocusEvent e) {
+                super.focusLost(e);
+                /*Se actualiza la cantidad*/
+            }
+        });
+    }
+    public void validarProducto(){
+        try{
+            if(tProducto.getText().isEmpty()){
+                throw new Exception();
+            }
+            Main.comprobarProducto(tProducto.getText());
+            System.out.println("funcion validarProducto: El producto es: " + tProducto.getText());
+            /*Aqui tengo que llamar a una funcion que me va a dar la informacion del importe para cargarlo en la ventana
+             * importe*/
+            tProducto.setEditable(false);
+            /*Me quedo con el producto*/
+            prodcutoSeleccionado = Main.getProduto(); /*En ProductoSeleccionado estoy guardando el producto que se ha
+            elegido en la ventana VENTA para poder cargar el importeUnitario en la ventana*/
+            System.out.println("ValidarProducto en Compra." + prodcutoSeleccionado.toString());
+
+        }catch (Exception e){
+            System.out.println(e.getClass()+ "Es un campo obligatorio");
+        }
     }
 }
--- TEMA7/ENTORNOGRAFICO_EJERCICIO3/src/VISTA/VentanaVenta.java
+++ TEMA7/ENTORNOGRAFICO_EJERCICIO3/src/VISTA/VentanaVenta.java
@@ -1,5 +1,6 @@
 package VISTA;
 
+import MODELO.Productos;
 import com.company.Main;
 
 import javax.swing.*;
@@ -18,20 +19,21 @@ public class VentanaVenta {
     private JLabel lPrecio;
     private JButton bAceptar;
     private JButton bCancelar;
-
+    static Productos prodcutoSeleccionado;/*Esta variable la uso en la f(x) comprobar producto para guardar el producto
+    que se ha elegido en la ventana VENTA*/
+     /*Ahora que ya tengo el producto guardado, quiero cargar en el textfield tPrecio el precio que tiene ese producto*/
     public JPanel getPanelPrincipal() { /*Para llamar desde el MAIN*/
         return panelPrincipal;
     }
 
-    public static void main(String[] args) { /*Copia Pega en el MAIN*/
+        public static void main(String[] args) { /*Copia Pega en el MAIN*/
         JFrame frame = new JFrame("VentanaVenta");
         frame.setContentPane(new VentanaVenta().panelPrincipal);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
     }
-
-    public VentanaVenta() {
+        public VentanaVenta() {
         tProducto.addFocusListener(new FocusAdapter() { /*Cuando pierde el foco que compruebe si el producto es valido.
         Que no esté vacio y que exista en el array*/
             @Override
@@ -41,34 +43,61 @@ public class VentanaVenta {
                 validarProducto();
             }
         });
-        tUnidades.addFocusListener(new FocusAdapter() {/*Que no está vacio y que hay suficientes unidades*/
 
-            @Override
-            public void focusLost(FocusEvent e) {
-                super.focusLost(e);
-            }
-        });
-        tPrecio.addFocusListener(new FocusAdapter() {/*Que no esté vacio y que el precio sea correcto. O tb puedo cargar el
-            precio que aparece en el objeto*/
-            @Override
-            public void focusLost(FocusEvent e) {
-                super.focusLost(e);
-            }
-        });
         bAceptar.addActionListener(new ActionListener() { /*Cuando se acepta o Cancela tiene que volver la pantalla opciones*/
             @Override
             public void actionPerformed(ActionEvent e) {
-                Main.mostrarVentanaOpciones();
-
+                /*Cuando hemos puesto el precio y damos a aceptar: Mostrar el importe de la venta.*/
+                calcularImporteVenta();
+                if (validarUnidades()){
+                    Main.ajustarStock(Integer.parseInt(tUnidades.getText()));
+                    /*Mostrar la ventanaOpciones va a ser lo ultimo que se haga*/
+                    tUnidades.setText("");
+                    tProducto.setText("");
+                    tPrecio.setText("");
+                    Main.mostrarVentanaOpciones();
+                }
             }
         });
         bCancelar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+
                 Main.mostrarVentanaOpciones();
             }
         });
+    }
+        public void calcularImporteVenta(){
+        try{
+            if(validarUnidades()){
+                float importeVenta = 0f;
+                importeVenta = Float.parseFloat(tUnidades.getText())*Float.parseFloat(tPrecio.getText());
+                JOptionPane.showMessageDialog(null,"El importe de la venta es: " + importeVenta);
+            }
 
+        }catch (Exception e){
+           System.out.println(e.getClass());;
+        }
+        }
+        public boolean validarUnidades(){
+        boolean validado =false;
+        try{
+            if(tUnidades.getText().isEmpty()){
+                System.out.println("El campo unidades esta vacio");
+                throw new Exception("El campo esta vacio");
+            }
+            else if(Integer.parseInt(tUnidades.getText())>prodcutoSeleccionado.getUnidades()){
+                JOptionPane.showMessageDialog(null, "No disponemos de tantas " + tProducto.getText()+
+                        "Lo maximo que puedes vender es: " + prodcutoSeleccionado.getUnidades());
+                tUnidades.setText("");
+            }
+            else{
+                validado =true;
+            }
+        }catch (Exception e){
+            System.out.println(e.getClass());
+        }
+        return validado;
         }
         public void validarProducto(){
         try{
@@ -76,8 +105,17 @@ public class VentanaVenta {
                 throw new Exception();
             }
             Main.comprobarProducto(tProducto.getText());
+            System.out.println("funcion validarProducto: El producto es: " + tProducto.getText());
             /*Aqui tengo que llamar a una funcion que me va a dar la informacion del importe para cargarlo en la ventana
             * importe*/
+            tProducto.setEditable(false);
+
+            prodcutoSeleccionado = Main.getProduto(); /*En ProductoSeleccionado estoy guardando el producto que se ha
+            elegido en la ventana VENTA para poder cargar el importeUnitario en la ventana*/
+            System.out.println("FUNCION GETPRODUCTO EN VENTANAVenta." + prodcutoSeleccionado.toString());
+            tPrecio.setText(Float.toString(prodcutoSeleccionado.getPrecioUnitario()));
+            tPrecio.setEditable(false);
+            /*Ahora me carga el precio y pone no editable los campos de precio y producto*/
 
         }catch (Exception e){
             System.out.println(e.getClass()+ "Es un campo obligatorio");
