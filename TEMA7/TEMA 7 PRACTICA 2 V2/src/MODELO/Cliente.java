3591f2a60096f76344ccaec0abf84173bb91da50
Celia Garcia <celia.garcia@ikasle.egibide.org>
2022-02-16 17:31:17 +0100

    EJERCICIO VERSION 1 COMPLETO

---
+++ TEMA7/TEMA 7 PRACTICA 2 V2/src/MODELO/Cliente.java
@@ -0,0 +1,18 @@
+package MODELO;
+
+public class Cliente {
+    String nombreCliente;
+
+
+    public Cliente(String nombreCliente) {
+        this.nombreCliente = nombreCliente;
+    }
+
+    public String getNombreCliente() {
+        return nombreCliente;
+    }
+
+    public void setNombreCliente(String nombreCliente) {
+        this.nombreCliente = nombreCliente;
+    }
+}
---
+++ TEMA7/TEMA 7 PRACTICA 2 V2/src/MODELO/Proveedor.java
@@ -0,0 +1,17 @@
+package MODELO;
+
+public class Proveedor {
+    String nombreProveedor;
+
+    public Proveedor(String nombreProveedor) {
+        this.nombreProveedor = nombreProveedor;
+    }
+
+    public String getNombreProveedor() {
+        return nombreProveedor;
+    }
+
+    public void setNombreProveedor(String nombreProveedor) {
+        this.nombreProveedor = nombreProveedor;
+    }
+}
